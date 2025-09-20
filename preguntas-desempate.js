6accfe5081a951c0fc909cdf49bab9524ecb4b56
Carlos Mondragon <carlos.mondragon@epicor.com>
2025-09-19 19:03:25 -0600

    first commit

---
+++ preguntas-desempate.js
@@ -0,0 +1,61 @@
+const defaultDataLastKill = [
+  {
+    pregunta: "Cual es el titulo del libro?", 
+    respuesta: "El Camino a Cristo"
+  },
+  {
+    pregunta: "Pregunta 2 ?", 
+    respuesta: "Respuesta 2"
+  },
+  {
+    pregunta: "Pregunta 3 ?", 
+    respuesta: "Respuesta 3"
+  },
+  {
+    pregunta: "Pregunta 4 ?", 
+    respuesta: "Respuesta 4"
+  },
+  {
+    pregunta: "Pregunta 5 ?", 
+    respuesta: "Respuesta 5"
+  },
+  {
+    pregunta: "Pregunta 6 ?", 
+    respuesta: "Respuesta 6"
+  },
+  {
+    pregunta: "Pregunta 7 ?", 
+    respuesta: "Respuesta 7"
+  },
+  {
+    pregunta: "Pregunta 8 ?", 
+    respuesta: "Respuesta 8"
+  },
+  {
+    pregunta: "Pregunta 9 ?", 
+    respuesta: "Respuesta 9"
+  },
+  {
+    pregunta: "Pregunta 10 ?", 
+    respuesta: "Respuesta 10"
+  },
+  {
+    pregunta: "Pregunta 11 ?", 
+    respuesta: "Respuesta 11"},
+  {
+    pregunta: "Pregunta 12 ?", 
+    respuesta: "Respuesta 12"
+  },
+  {
+    pregunta: "Pregunta 13 ?", 
+    respuesta: "Respuesta 13"
+  },
+  {
+    pregunta: "Pregunta 14 ?", 
+    respuesta: "Respuesta 14"
+  },
+  {
+    pregunta: "Pregunta 15 ?", 
+    respuesta: "Respuesta 15"
+  }
+];
---
+++ preguntas.js
@@ -0,0 +1,121 @@
+const defaultData = [
+  {
+    pregunta: "Cual es el titulo del libro?", 
+    respuesta: "El Camino a Cristo"
+  },
+  {
+    pregunta: "Pregunta 2 ?", 
+    respuesta: "Respuesta 2"
+  },
+  {
+    pregunta: "Pregunta 3 ?", 
+    respuesta: "Respuesta 3"
+  },
+  {
+    pregunta: "Pregunta 4 ?", 
+    respuesta: "Respuesta 4"
+  },
+  {
+    pregunta: "Pregunta 5 ?", 
+    respuesta: "Respuesta 5"
+  },
+  {
+    pregunta: "Pregunta 6 ?", 
+    respuesta: "Respuesta 6"
+  },
+  {
+    pregunta: "Pregunta 7 ?", 
+    respuesta: "Respuesta 7"
+  },
+  {
+    pregunta: "Pregunta 8 ?", 
+    respuesta: "Respuesta 8"
+  },
+  {
+    pregunta: "Pregunta 9 ?", 
+    respuesta: "Respuesta 9"
+  },
+  {
+    pregunta: "Pregunta 10 ?", 
+    respuesta: "Respuesta 10"
+  },
+  {
+    pregunta: "Pregunta 11 ?", 
+    respuesta: "Respuesta 11"},
+  {
+    pregunta: "Pregunta 12 ?", 
+    respuesta: "Respuesta 12"
+  },
+  {
+    pregunta: "Pregunta 13 ?", 
+    respuesta: "Respuesta 13"
+  },
+  {
+    pregunta: "Pregunta 14 ?", 
+    respuesta: "Respuesta 14"
+  },
+  {
+    pregunta: "Pregunta 15 ?", 
+    respuesta: "Respuesta 15"
+  },
+  {
+    pregunta: "Pregunta 16 ?", 
+    respuesta: "Respuesta 16"
+  },
+  {
+    pregunta: "Pregunta 17 ?", 
+    respuesta: "Respuesta 17"
+  },
+  {
+    pregunta: "Pregunta 18 ?", 
+    respuesta: "Respuesta 18"
+  },
+  {
+    pregunta: "Pregunta 19 ?", 
+    respuesta: "Respuesta 19"
+  },
+  {
+    pregunta: "Pregunta 20 ?", 
+    respuesta: "Respuesta 20"
+  },
+  {
+    pregunta: "Pregunta 21 ?", 
+    respuesta: "Respuesta 21"
+  },
+  {
+    pregunta: "Pregunta 22 ?", 
+    respuesta: "Respuesta 22"
+  },
+  {
+    pregunta: "Pregunta 23 ?", 
+    respuesta: "Respuesta 23"
+  },
+  {
+    pregunta: "Pregunta 24 ?", 
+    respuesta: "Respuesta 24"
+  },
+  {
+    pregunta: "Pregunta 25 ?", 
+    respuesta: "Respuesta 25"
+  },
+  {
+    pregunta: "Pregunta 26 ?", 
+    respuesta: "Respuesta 26"
+  },
+  {
+    pregunta: "Pregunta 27 ?", 
+    respuesta: "Respuesta 27"
+  },
+  {
+    pregunta: "Pregunta 28 ?", 
+    respuesta: "Respuesta 28"
+  },
+  {
+    pregunta: "Pregunta 29 ?", 
+    respuesta: "Respuesta 29"
+  },
+  {
+    pregunta: "Quien escribio el libro?", 
+    respuesta: "Ellen G. White"
+  }
+];
